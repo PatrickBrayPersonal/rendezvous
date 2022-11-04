4375ab3ea22a15f612444c040e73be29faa64d8a
Patrick Bray <patrickbray17@gmail.com>
2022-11-04 14:37:53 -0400

    make script importable

--- scripts/places_json_processor.py
+++ scripts/places_json_processor.py
@@ -37,27 +37,29 @@ def create_edge(previous_nodes, business, edges, number):#creates edges using th
 
 
 
-files = ['places_movie.json', 'places_bar.json', 'places_restaurants.json'] #test json files
 
-businesses = [{'placeId': 1, 'type': 'user_node', 'rating': False, 'distance': False, 'location': {'lat': 38, 'lng': -77}}] #format of business list-dict structure and User data
-nodes = [{'placeId': 1, 'type': 'user_node', 'rating': False}] #format for nodes list-dict structure and User data
-edges = [] # empty list to hold edges, passed in edges function and appended to
-previous_nodes = [{'placeId':1, 'type': 'user_node', 'rating': False}] #list to hold the nodes from the previous API call
+if __name__ == "__main__":
+    files = ['places_movie.json', 'places_bar.json', 'places_restaurants.json'] #test json files
 
-number = 2 #ID for nodes
+    businesses = [{'placeId': 1, 'type': 'user_node', 'rating': False, 'distance': False, 'location': {'lat': 38, 'lng': -77}}] #format of business list-dict structure and User data
+    nodes = [{'placeId': 1, 'type': 'user_node', 'rating': False}] #format for nodes list-dict structure and User data
+    edges = [] # empty list to hold edges, passed in edges function and appended to
+    previous_nodes = [{'placeId':1, 'type': 'user_node', 'rating': False}] #list to hold the nodes from the previous API call
 
-for file in files:
-	jsonObject = load_json(file)
-	prev_nodes = [] # empty list that will be populated with nodes from this loops run
-	for business in jsonObject['results']: #loop through json results 
-		if business['business_status'] != 'OPERATIONAL': #Check that the business is operating 
-			continue
-		businesses.append(create_business(business, number))#appends businesses created from json object to the businesses list in the paremeters
-		nodes.append(create_nodes(business, number)) #appends nodes created from the json object to the nodes list in the paremeters
-		prev_nodes.append(create_nodes(business, number)) #maps nodes from this json object to the nodes list in the parameters 
-		create_edge(previous_nodes,business,edges, number)#appends edges from the
-		number += 1
-	previous_nodes = prev_nodes.copy() #overwrites the previous_nodes variable with the nodes from this loop run 
+    number = 2 #ID for nodes
+
+    for file in files:
+        jsonObject = load_json(f"test_data/{file}")
+        prev_nodes = [] # empty list that will be populated with nodes from this loops run
+        for business in jsonObject['results']: #loop through json results 
+            if business['business_status'] != 'OPERATIONAL': #Check that the business is operating 
+                continue
+            businesses.append(create_business(business, number))#appends businesses created from json object to the businesses list in the paremeters
+            nodes.append(create_nodes(business, number)) #appends nodes created from the json object to the nodes list in the paremeters
+            prev_nodes.append(create_nodes(business, number)) #maps nodes from this json object to the nodes list in the parameters 
+            create_edge(previous_nodes,business,edges, number)#appends edges from the
+            number += 1
+        previous_nodes = prev_nodes.copy() #overwrites the previous_nodes variable with the nodes from this loop run 
 
 
 
